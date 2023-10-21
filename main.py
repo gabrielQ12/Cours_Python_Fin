2772dbba371d7eb01d666a6a6374e5ec77798471
gabrielQ12 <gabriel.quinette1@gmail.com>
2023-10-21 11:37:14 +0200

    initiation_Mechanize

--- main.py
+++ main.py
@@ -5,16 +5,17 @@ import mechanize
 from bs4 import BeautifulSoup
 
 
-def view_page(url, proxy):
+def view_page(url, proxy=None):
     browser = mechanize.Browser()
     browser.set_handle_robots(False)
     user_agent = [("User-agent", "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0")]
     browser.addheaders = user_agent
-    browser.set_proxies(proxy)
+    if proxy:
+        browser.set_proxies(proxy)
     page = browser.open(url)
     for cookie in browser.cookiejar:
         print(cookie)
-        #print(page.read())
+    print(page.read())
 
 
-view_page('https://icanhazip.com')
+view_page('https://icanhazip.com') #{"https" : "212.34.250.14:3128"}
