707d60853c9088770489c7a5cfe1cd416e894aba
gabrielQ12 <gabriel.quinette1@gmail.com>
2023-10-21 10:52:04 +0200

    viewpage

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python3
 # coding: utf-8
+
+import mechanize
+from bs4 import BeautifulSoup
+
+
+def view_page(url, proxy):
+    browser = mechanize.Browser()
+    browser.set_handle_robots(False)
+    user_agent = [("User-agent", "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0")]
+    browser.addheaders = user_agent
+    browser.set_proxies(proxy)
+    page = browser.open(url)
+    for cookie in browser.cookiejar:
+        print(cookie)
+        #print(page.read())
+
+
+view_page('https://icanhazip.com')
